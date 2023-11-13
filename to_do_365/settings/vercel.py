e5b1c41ba64758fd9f4f7739b39c08337c23aee9
Gabriel Aguiar <lugafeagre@gmail.com>
2023-11-13 20:23:58 -0300

    Trying to fix vercel deploy

--- to_do_365/settings/vercel.py
+++ to_do_365/settings/vercel.py
@@ -2,3 +2,14 @@ from to_do_365.settings.base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['.vercel.app']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo365',
+        'USER': 'adminposte',
+        'PASSWORD': 'hXkfuuJLwuuloHuMWxyDaLnSfEuZNqz0',
+        'HOST': 'dpg-cl9amfto7jlc73b0c78g-a',
+        'PORT': '5432',
+    }
+}
--- to_do_365/urls.py
+++ to_do_365/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+# from django.conf import settings
+# from django.conf.urls.static import static
 from django.urls import include, path
 
 from to_do_list_with_calendar.urls import urlpatterns as to_do_urls
@@ -7,3 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(to_do_urls)),
 ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
