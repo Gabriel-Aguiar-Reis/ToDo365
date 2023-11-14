116820eaee0600d0b51b6654515a5296c27227ea
Gabriel Aguiar <lugafeagre@gmail.com>
2023-11-14 16:16:46 -0300

    starting to implement tests

--- to_do_list_with_calendar/tests.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from rest_framework.test import APITestCase
-
-
-class TestListUsuarios(APITestCase):
-    def test_listagem_vazia(self):
-        response = self.client.get('/api/usuarios/')
-        self.assertEqual(json.loads(response.content), [])
