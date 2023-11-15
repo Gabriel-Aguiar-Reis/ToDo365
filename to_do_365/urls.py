6bd6d2dd46ed1fae3403c13a33ca58ab5dbc817d
Gabriel Aguiar <lugafeagre@gmail.com>
2023-11-15 01:21:59 -0300

    fixing deploy build bug

--- to_do_365/urls.py
+++ to_do_365/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 from to_do_list_with_calendar.urls import urlpatterns as to_do_urls
-from to_do_list_with_calendar.views import DocumentationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
