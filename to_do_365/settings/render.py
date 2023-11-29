149e4e324acbd30c5f55d7b6fdbbd6b816a2401e
Gabriel Aguiar Reis <lugafeagre@gmail.com>
2023-11-29 01:12:31 -0300

    forbid debug

--- to_do_365/settings/render.py
+++ to_do_365/settings/render.py
@@ -2,8 +2,7 @@ import dj_database_url
 
 from to_do_365.settings.base import *
 
-DEBUG = True
-# DEBUG = 'RENDER' not in os.environ
+DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
